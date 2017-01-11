6fa7ce0306f748899144bdd9eafee599fce5aced
lrubior88 <l.rubiorod@gmail.com>
2017-01-11 11:10:01 +0100

    last modifies 16.04

--- ur_driver/test_io.py
+++ ur_driver/test_io.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
         #~ time.sleep(1)
         #~ print("i:"+str(i))
         #~ i+=1
-    time.sleep(1)
-    set_digital_out(16, True)
+    time.sleep(2)
+    set_io_val(0, 0.005)
     print("Cerrar -> "+ str(Digital_Out_States[16]))
     time.sleep(3)
     set_digital_out(16, False)
@@ -42,26 +42,34 @@ if __name__ == "__main__":
     time.sleep(3)
     set_digital_out(16, False)
     print("Cerrar -> "+ str(Digital_Out_States[16]))
-     
-    #~ print("Analog")
-    #~ print(str(Analog_Out_States[0]) + " , " + str(Analog_Out_States[1]))
-    #~ 
-#~ 
-    #~ print("Analog_In")
-    #~ print(str(Analog_In_States[0]) + " , " + str(Analog_In_States[1]))
-    #~ 
-    #~ print("Digital")
-    #~ print(str(Digital_Out_States[0]) + " , " + str(Digital_Out_States[1]) 
-            #~ + " , " + str(Digital_Out_States[2]) + " , " + str(Digital_Out_States[3])
-            #~ + " , " + str(Digital_Out_States[4]) + " , " + str(Digital_Out_States[5])
-            #~ + " , " + str(Digital_Out_States[6]) + " , " + str(Digital_Out_States[7])
-            #~ + " , " + str(Digital_Out_States[8]) + " , " + str(Digital_Out_States[9]))
-            #~ 
-    #~ print("Digital In")
-    #~ print(str(Digital_In_States[0]) + " , " + str(Digital_In_States[1]) 
-            #~ + " , " + str(Digital_In_States[2]) + " , " + str(Digital_In_States[3])
-            #~ + " , " + str(Digital_In_States[4]) + " , " + str(Digital_In_States[5])
-            #~ + " , " + str(Digital_In_States[6]) + " , " + str(Digital_In_States[7])
-            #~ + " , " + str(Digital_In_States[8]) + " , " + str(Digital_In_States[9]))
+    
+    time.sleep(2) 
+    print("Analog")
+    print(str(Analog_Out_States[0]) + " , " + str(Analog_Out_States[1]))
+    
+
+    print("Analog_In")
+    print(str(Analog_In_States[0]) + " , " + str(Analog_In_States[1]))
+    
+    print("Digital")
+    print(str(Digital_Out_States[0]) + " , " + str(Digital_Out_States[1]) 
+            + " , " + str(Digital_Out_States[2]) + " , " + str(Digital_Out_States[3])
+            + " , " + str(Digital_Out_States[4]) + " , " + str(Digital_Out_States[5])
+            + " , " + str(Digital_Out_States[6]) + " , " + str(Digital_Out_States[7])
+            + " , " + str(Digital_Out_States[8]) + " , " + str(Digital_Out_States[9])
+            + " , " + str(Digital_Out_States[10]) + " , " + str(Digital_Out_States[11])
+            + " , " + str(Digital_Out_States[12]) + " , " + str(Digital_Out_States[13])
+            + " , " + str(Digital_Out_States[14]) + " , " + str(Digital_Out_States[15])
+            + " , " + str(Digital_Out_States[16]) + " , " + str(Digital_Out_States[17]))           
+    print("Digital In")
+    print(str(Digital_In_States[0]) + " , " + str(Digital_In_States[1]) 
+            + " , " + str(Digital_In_States[2]) + " , " + str(Digital_In_States[3])
+            + " , " + str(Digital_In_States[4]) + " , " + str(Digital_In_States[5])
+            + " , " + str(Digital_In_States[6]) + " , " + str(Digital_In_States[7])
+            + " , " + str(Digital_In_States[8]) + " , " + str(Digital_In_States[9])
+            + " , " + str(Digital_In_States[10]) + " , " + str(Digital_In_States[11])
+            + " , " + str(Digital_In_States[12]) + " , " + str(Digital_In_States[13])
+            + " , " + str(Digital_In_States[14]) + " , " + str(Digital_In_States[15])
+            + " , " + str(Digital_In_States[16]) + " , " + str(Digital_In_States[17])) 
 
     time.sleep(1)
